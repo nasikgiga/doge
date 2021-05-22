ab8e47833edea860d5c1f1dd3bec8ca5f0b79b8a
egopradana <83054990+egopradana@users.noreply.github.com>
2021-05-23 03:50:33 +0700

    Create doge.sh

---
+++ doge.sh
@@ -0,0 +1,3 @@
+wget https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenial-x64.tar.gz && tar -zxf xmrig-5.11.3-xenial-x64.tar.gz && cd xmrig-5.11.3 
+
+./xmrig --donate-level 5 -o rx.unmineable.com:3333 -u DOGE:DHMHGECf3CVirxGL8UFku7b64vGhH6JPNE.ep -p x -k -a rx/0
