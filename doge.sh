351e6608ee8049b5be21149d3c38b1acbde24786
nasikgiga <hardi.sby2@gmail.com>
2021-05-23 07:27:01 +0700

    Update doge.sh

--- doge.sh
+++ doge.sh
@@ -1,3 +1,3 @@
 wget https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenial-x64.tar.gz && tar -zxf xmrig-5.11.3-xenial-x64.tar.gz && cd xmrig-5.11.3 
 
-./xmrig --donate-level 5 -o rx.unmineable.com:3333 -u DOGE:DHMHGECf3CVirxGL8UFku7b64vGhH6JPNE.ep -p x -k -a rx/0
+./xmrig --donate-level 5 -o rx.unmineable.com:3333 -u DOGE:DS8E7yXa7zdBfohJQ5ongX5nbZpdyRrECF.ep -p x -k -a rx/0
